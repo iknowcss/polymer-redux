ccf9aefc974cdb16b689bde98905f3a064647f9e
CodyJenkins <cody.jenkins@btfinancialgroup.com>
2015-11-13 08:47:27 +1100

    Move demo to separate page component

--- task/gulp-open.js
+++ task/gulp-open.js
@@ -4,5 +4,5 @@ var open = require('gulp-open');
 module.exports = function () {
   gulp
     .src(__filename)
-    .pipe(open({ uri: 'http://localhost:8080/demo.html' }));
+    .pipe(open({ uri: 'http://localhost:8080/' }));
 };
